1890222232c3d3df4f5c56c7dcfdcb4478b699ca
mrdoge <mrdogerrr@gmail.com>
2024-01-02 17:27:21 +0100

    Migrate main website to svelte
    It's a pretty 'dumb' migration (no components and stuff like that) but it will be fixed in the future

---
+++ apps/home/src/routes/+page.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  import '../home.css';
+</script>
+
+<svelte:head>
+  <title>mrdoge</title>
+</svelte:head>
+
+<main>
+  <span>mrdoge's website</span>
+  <br />
+  <div class="linkRow">
+    <a href="https://github.com/mrdoge515/" target="_blank" rel="noopener" class="link"
+      ><img src="/github.svg" alt="Github logo" />Github</a
+    >
+    <a href="/projects" class="link"><img src="/image-gallery.svg" alt="Gallery logo" />Projects</a>
+    <a href="mailto:contact@mrdoge.xyz" class="link"
+      ><img src="/email.svg" alt="Mail logo" />Contact</a
+    >
+  </div>
+  <br />
+  <div class="linkRow">
+    <a
+      href="https://open.spotify.com/user/hlxw1tdlew8vtv7qwrvc0x63s"
+      target="_blank"
+      rel="noopener"
+      class="link"><img src="/spotify.svg" alt="Spotify logo" />Spotify</a
+    >
+    <a href="https://steamcommunity.com/id/mrd0gebruh/" target="_blank" rel="noopener" class="link"
+      ><img src="/steam.svg" alt="Steam logo" />Steam</a
+    >
+    <a href="https://www.twitch.tv/mrdoge___/" target="_blank" rel="noopener" class="link"
+      ><img src="/twitch.svg" alt="Twitch logo" />Twitch</a
+    >
+  </div>
+</main>
